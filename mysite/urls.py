e0098f2e1b4fed90568d305801a6358c66788a88
xiaodezhang <zhangxiaode0@gmail.com>
2019-04-17 15:00:18 +0800

    1,add other apps's urls and view file

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-#from . import views
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.index ),
     path('note/', include('note.urls')),
+    path('blog/', include('blog.urls')),
+    path('dic/', include('dic.urls')),
 ]
