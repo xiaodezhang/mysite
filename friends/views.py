035a8c7587a9fd2c87ad56d5025d5c3fc46e9768
xiaodezhang <zhangxiaode0@gmail.com>
2019-06-28 18:39:11 +0800

    a script to get all videos in certen path

--- friends/views.py
+++ friends/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+import os
 
 # Create your views here.
 def index(request):
-    return render(request,'friends/vidoes.html')
+    video_path = "."+static('friends/videos')
+    video_list = []
+    for video_dir in os.listdir(video_path):
+        video_path_full = video_path+'/'+video_dir
+        if os.path.isdir(video_path_full):
+            episode = []
+            for episode_name in os.listdir(video_path+"/"+video_dir):
+                episode_name_full = video_path_full+"/"+episode_name
+                if os.path.isfile(episode_name_full):
+                    episode_num = episode_name.split('_')
+                    episode.append({'episode':episode_num[1],'urlpath':'friends/videos'+video_dir+"/"+episode_name})
+            video_list.append({'section':video_dir,'episode':episode})
+    return render(request,'friends/video.html',{"video_list":video_list})
